6ce4ef0fd0bbf8e0059fa8e2f09a0e461a2a0e1c
jtheoc80 <jtheoc80@gmail.com>
2024-11-02 06:20:45 -0500

    Create search.js

---
+++ pages/search.js
@@ -0,0 +1,12 @@
+// pages/search.js
+
+import React from 'react';
+import SearchResults from '../components/SearchResults';
+
+export default function SearchPage() {
+  return (
+    <div>
+      <SearchResults />
+    </div>
+  );
+}
